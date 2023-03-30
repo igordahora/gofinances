c38f2e3ec5fea4fb62b4e9b4947cbd089cc84e5d
Igor da Hora <igordahora@gmail.com>
2023-03-30 17:29:30 -0300

    Adicionando cartões de destaque e iniciando cartões de transação

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,37 +1,51 @@
 import React from 'react';
 
-import { Feather } from '@expo/vector-icons'
+import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard } from '../../components/TransactionCard';
 
-import { 
-    Container, 
+import {
+    Container,
     Header,
     UserWrapper,
     UserInfo,
     Photo,
     User,
     UserGreeting,
-    UserName
+    UserName,
+    HighlightCards,
+    Icon,
+    Transactions,
+    Title
 } from './styles';
-import { HighlightCard } from '../../components/HighlightCard';
 
-export function Dashboard(){
+
+
+export function Dashboard() {
     return (
         <Container>
             <Header>
-
                 <UserWrapper>
                     <UserInfo>
-                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/4870136?v=4'}} />
+                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/4870136?v=4' }} />
                         <User>
                             <UserGreeting>Olá</UserGreeting>
                             <UserName>Igor</UserName>
                         </User>
                     </UserInfo>
+                    <Icon name="power" />
                 </UserWrapper>
-                <Feather name="power"/>
             </Header>
 
-            <HighlightCard></HighlightCard>
+            <HighlightCards>
+                <HighlightCard title='Entradas' amount='R$ 17.400,00' lastTransaction='Última entrada dia 13 de abril' type="up"></HighlightCard>
+                <HighlightCard title='Saídas' amount='R$ 1.259,00' lastTransaction='Última saída dia 03 de abril' type="down"></HighlightCard>
+                <HighlightCard title='Total' amount='R$ 16.141,00' lastTransaction='01 à 16 de abril' type="total"></HighlightCard>
+            </HighlightCards>
+
+            <Transactions>
+                <Title>Listagem</Title>
+                <TransactionCard />
+            </Transactions>
         </Container>
     )
 }
