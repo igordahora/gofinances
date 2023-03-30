091a310f96505ca7488d3ce1964db5bd99c0ab0a
Igor da Hora <igordahora@gmail.com>
2023-03-30 00:32:17 -0300

    Start componente HighlightCard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import { Feather } from '@expo/vector-icons'
+
+import { 
+    Container, 
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName
+} from './styles';
+import { HighlightCard } from '../../components/HighlightCard';
 
 export function Dashboard(){
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/4870136?v=4'}} />
+                        <User>
+                            <UserGreeting>Olá</UserGreeting>
+                            <UserName>Igor</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+                <Feather name="power"/>
+            </Header>
+
+            <HighlightCard></HighlightCard>
         </Container>
     )
 }
