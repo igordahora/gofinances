020075d9a5ee78933a070ab05a2c776a4c294aa2
Igor da Hora <igordahora@gmail.com>
2023-04-02 23:48:14 -0300

    Iniciando modulo de registro

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { 
+    Container,
+    Header,
+    Title 
+} from './styles';
+
+export function Register(){
+    return (
+        <Container>
+            <Header>
+                <Title>registro</Title>
+            </Header>
+        </Container>
+    )
+}
---
+++ src/screens/Register/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+export const Container = styled.View``
+
+export const Header = styled.View`
+    background-color: ${({ theme }) => theme.colors.primary};
+    width: 100%;
+    height: ${RFValue(113)}px;
+
+    align-items: center;
+    
+`;
+
+export const Title = styled.Text``;
