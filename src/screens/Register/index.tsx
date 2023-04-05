d223c17338c7139039659fcf40cb207072e15f76
Igor da Hora <igordahora@gmail.com>
2023-04-04 23:30:40 -0300

    Add Components de Input

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 import { 
     Container,
+    Fields,
+    Form,
     Header,
     Title 
 } from './styles';
+
+import { Input } from '../../components/Form/Input';
+import { Button } from '../../components/Form/Button';
 
 export function Register(){
     return (
@@ -12,6 +17,17 @@ export function Register(){
             <Header>
                 <Title>registro</Title>
             </Header>
+            <Form>
+                <Fields>
+                    <Input
+                        placeholder='Nome' 
+                    />
+                    <Input
+                        placeholder='Preço' 
+                    />
+                </Fields>
+                <Button title='Enviar'/>
+            </Form>
         </Container>
     )
 }
