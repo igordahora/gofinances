cb5ec44a14c704c8dc13c8c6cbbcf06fbf7325d2
Igor da Hora <igordahora@gmail.com>
2023-04-14 00:07:38 -0300

    Ajustando formatação

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -4,84 +4,101 @@ import { HighlightCard } from '../../components/HighlightCard';
 import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
 
 import {
-    Container,
-    Header,
-    UserWrapper,
-    UserInfo,
-    Photo,
-    User,
-    UserGreeting,
-    UserName,
-    HighlightCards,
-    Icon,
-    Transactions,
-    Title,
-    TransactionList
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  HighlightCards,
+  Icon,
+  Transactions,
+  Title,
+  TransactionList
 } from './styles';
 
 
 export interface DataListProps extends TransactionCardProps {
-    id: string;
+  id: string;
 }
 
 export function Dashboard() {
 
     const data: DataListProps[] = [
-        {
-            id: '1',
-            type: 'positive',
-            title: 'Desenvolvimento de site',
-            amount: 'R$ 12.000,00',
-            category: {name:'Vendas', icon: 'dollar-sign'},
-            date: "13/04/2020"
-        },
-        {
-            id: '2',
-            type: 'negative',
-            title: 'Hamburgueria Pizzy',
-            amount: 'R$ 59,00',
-            category: {name:'Alimentação', icon: 'coffee'},
-            date: "10/04/2020"
-        },
-        {
-            id: '3',
-            type: 'negative',
-            title: 'Aluguel do Apartamento',
-            amount: 'R$ 1.200,00',
-            category: {name:'Casa', icon: 'shopping-bag'},
-            date: "10/04/2020"
-        }
+      {
+          id: '1',
+          type: 'positive',
+          title: 'Desenvolvimento de site',
+          amount: 'R$ 12.000,00',
+          category: {name:'Vendas', icon: 'dollar-sign'},
+          date: "13/04/2020"
+      },
+      {
+          id: '2',
+          type: 'negative',
+          title: 'Hamburgueria Pizzy',
+          amount: 'R$ 59,00',
+          category: {name:'Alimentação', icon: 'coffee'},
+          date: "10/04/2020"
+      },
+      {
+          id: '3',
+          type: 'negative',
+          title: 'Aluguel do Apartamento',
+          amount: 'R$ 1.200,00',
+          category: {name:'Casa', icon: 'shopping-bag'},
+          date: "10/04/2020"
+      }
     ];
 
     return (
-        <Container>
-            <Header>
-                <UserWrapper>
-                    <UserInfo>
-                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/4870136?v=4' }} />
-                        <User>
-                            <UserGreeting>Olá</UserGreeting>
-                            <UserName>Igor</UserName>
-                        </User>
-                    </UserInfo>
-                    <Icon name="power" />
-                </UserWrapper>
-            </Header>
+      <Container>
+          <Header>
+              <UserWrapper>
+                  <UserInfo>
+                      <Photo
+                        source={{ uri: 'https://avatars.githubusercontent.com/u/4870136?v=4' }}
+                      />
+                      <User>
+                          <UserGreeting>Olá</UserGreeting>
+                          <UserName>Igor</UserName>
+                      </User>
+                  </UserInfo>
+                  <Icon name="power" />
+              </UserWrapper>
+          </Header>
 
-            <HighlightCards>
-                <HighlightCard title='Entradas' amount='R$ 17.400,00' lastTransaction='Última entrada dia 13 de abril' type="up"></HighlightCard>
-                <HighlightCard title='Saídas' amount='R$ 1.259,00' lastTransaction='Última saída dia 03 de abril' type="down"></HighlightCard>
-                <HighlightCard title='Total' amount='R$ 16.141,00' lastTransaction='01 à 16 de abril' type="total"></HighlightCard>
-            </HighlightCards>
+          <HighlightCards>
+              <HighlightCard
+                title='Entradas'
+                amount='R$ 17.400,00'
+                lastTransaction='Última entrada dia 13 de abril'
+                type="up"
+              ></HighlightCard>
+              <HighlightCard
+                title='Saídas'
+                amount='R$ 1.259,00'
+                lastTransaction='Última saída dia 03 de abril'
+                type="down"
+              ></HighlightCard>
+              <HighlightCard
+                title='Total'
+                amount='R$ 16.141,00'
+                lastTransaction='01 à 16 de abril'
+                type="total"
+              ></HighlightCard>
+          </HighlightCards>
 
-            <Transactions>
-                <Title>Listagem</Title>
-                <TransactionList
-                    data={ data }
-                    keyExtractor={ item => item.id }
-                    renderItem={({ item }) => <TransactionCard data={ item } />}
-                />
-            </Transactions>
-        </Container>
+          <Transactions>
+              <Title>Listagem</Title>
+              <TransactionList
+                  data={ data }
+                  keyExtractor={ item => item.id }
+                  renderItem={({ item }) => <TransactionCard data={ item } />}
+              />
+          </Transactions>
+      </Container>
     )
 }
